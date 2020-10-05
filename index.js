80dcf94055603ca6f4203d87c14506886ad7adde
Ru <rudikraeher@gmail.com>
2020-10-05 15:15:15 -0700

    created a description heading in the index.js file template

--- index.js
+++ index.js
@@ -3,63 +3,64 @@ const inquirer = require("inquirer");
 const todaysDate = new Date();
 const year = todaysDate.getFullYear();
 
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-        type: 'input',
-        message: 'For licensing purposes, what is your name?',
-        name: 'fullname'
+      type: "input",
+      message: "For licensing purposes, what is your name?",
+      name: "fullname",
     },
     {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'github'
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
     },
     {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email'
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
     },
     {
-        type: 'input',
-        message: 'What is your project title?',
-        name: 'title'
+      type: "input",
+      message: "What is your project title?",
+      name: "title",
     },
     {
-        type: 'input',
-        message: 'Provide a brief description of the project: ',
-        name: 'description'
+      type: "input",
+      message: "Provide a brief description of the project: ",
+      name: "description",
     },
     {
-        type: 'input',
-        message: 'Provide installation instructions: ',
-        name: 'install'
+      type: "input",
+      message: "Provide installation instructions: ",
+      name: "install",
     },
     {
-        type: 'input',
-        message: 'Provide usage information: ',
-        name: 'usage'
+      type: "input",
+      message: "Provide usage information: ",
+      name: "usage",
     },
     {
-        type: 'input',
-        message: 'Provide the guidelines for contributing to the project: ',
-        name: 'contributing'
+      type: "input",
+      message: "Provide the guidelines for contributing to the project: ",
+      name: "contributing",
     },
     {
-        type: 'input',
-        message: 'Provide instructions for testing the project: ',
-        name: 'tests'
+      type: "input",
+      message: "Provide instructions for testing the project: ",
+      name: "tests",
     },
     {
-        type: 'list',
-        message: 'Select a license for your application.',
-        name: 'license',
-        choices: ['MIT', 'ISC', 'GNU', 'Apache']
+      type: "list",
+      message: "Select a license for your application.",
+      name: "license",
+      choices: ["MIT", "ISC", "GNU", "Apache"],
     },
+  ])
+  .then(function (data) {
+    let readme = `# ${data.title}
 
-]).then(function (data) {
-  let readme = 
-    `# ${data.title}
-    
+## Description
     ${data.description}
     
 ## Table of Contents 
@@ -88,10 +89,12 @@ inquirer.prompt([
     
 ## Questions
     You can reach me at ${data.email}
-    Visit my [GitHub] ${data.github}`
+    Visit my [GitHub] ${data.github}`;
 
-  fs.writeFile("good-readme.md", readme, function (err) {
-    if (err) throw err;
-    console.log(`You have successfully generated a good README file.`);
+    fs.writeFile("README.md", readme, function (err) {
+      if (err) throw err;
+      console.log(
+        `You have successfully generated a good README.md file. Please check the current directory for your new README.`
+      );
+    });
   });
-});
